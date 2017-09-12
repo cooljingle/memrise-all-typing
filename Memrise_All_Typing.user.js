478e1b7215d7b7a84a5f4a959079db983c4c75ed
Weyland Joyner <weylandjoyner4@gmail.com>
2017-09-12 18:55:32 -0400

    added pattern for final space

--- Memrise_All_Typing.user.js
+++ Memrise_All_Typing.user.js
@@ -179,7 +179,7 @@ $(document).ready(function() {
                     },
                     correct: v.item.value,
                     choices: "",
-                    accepted: _.map(v.item.alternatives.concat(v.item.value), function(x) {return x.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~¿¡]/g, "");}) //strip punctuation
+                    accepted: _.map(v.item.alternatives.concat(v.item.value), function(x) {return x.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~¿¡]|\s\./g, "");}) //strip punctuation
                 };
             }
         });
